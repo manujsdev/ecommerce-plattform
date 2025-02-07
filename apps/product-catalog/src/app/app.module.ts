9912604c01dc3de8d4ab6507bba11a85e6e07dea
Manu <manujsdev@gmail.com>
2025-02-07 15:25:39 -0500

    feat(task#2): added configs

--- apps/product-catalog/src/app/app.module.ts
+++ apps/product-catalog/src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../config/configuration';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
